5f2c631531c8f6f64b53e5357b8fd9cdaee909cd
rizhou <rizhou@buyercamp.com>
2018-10-12 17:37:29 +0800

    test

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -38,7 +38,7 @@ class test extends Command
      */
     public function handle()
     {
-        for($i=19314;$i<=20000;$i++){
+        for($i=20001;$i<=83798;$i++){
             try{
                 $LewenRule = new LewenRule("http://www.lewen123.com/lewen/$i.html");
                 $LewenRule->request()->get();
